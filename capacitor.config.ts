e3cf7a432ab171159dc2554938242fde73b99c4b
Andre <arc.xoc@gmail.com>
2022-02-08 00:32:48 -0800

    Added Description to capcitor 📝

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,11 @@
+//update app name and appID here
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.rockettemplate.app',
   appName: 'Rocket Template',
   webDir: 'build',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
 };
 
 export default config;
